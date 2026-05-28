6dd7a3710606c3cba6ca4ddcbb2f229d5cf2880d
Dumitru Stefan-Fernando <dumitrustefanfernando@outlook.com>
2026-05-28 23:37:48 +0200

    display vacancy list on home and single vacancy detail view

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,16 @@
+const Vacante = require('../models/Vacantes');
 
 exports.mostrarTrabajos = async (req, res, next) => {
+
+    const vacantes = await Vacante.find().lean();
+
+    if(!vacantes) return next();
 
     res.render('home', {
         nombrePagina : 'devJobs',
         tagline: 'Find and Post Jobs for Web Developers',
         barra: true,
         boton: true,
-    })
+        vacantes
+    });
 }
